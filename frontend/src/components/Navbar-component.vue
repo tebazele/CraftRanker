fd89888e1d2af4b9ca2c85d827286e4aa547e426
Jeanne Allen <tebazele@gmail.com>
2023-08-25 14:24:36 -0600

    vue 3 pinia bootstrap installed via npm Project is ready for UI dev. scss file not recognized,. main.css is styling everything currently.

---
+++ frontend/src/components/Navbar-component.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="bg-header p-5">
+    <div class="d-flex flex-column">
+      <img alt="logo" src="../assets/img/crLogoSquare.svg" height="120" />
+    </div>
+  </nav>
+</template>
+
+
+<script>
+// import { computed, reactive, onMounted } from 'vue';
+export default {
+  setup() {
+    return {}
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.bg-header {
+  background-image: url('../assets/img/headerLongDarker.jpg');
+  background-position: center;
+  background-size: cover;
+}
+</style>
---
+++ frontend/src/stores/auth.js
@@ -0,0 +1,14 @@
+// import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('auth', {
+  state: {
+
+  },
+  getters: {
+
+  },
+  actions: {
+
+  }
+})
