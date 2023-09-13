a3ae324e46c842a0e4e283599d005521981cd614
Jeanne Allen <tebazele@gmail.com>
2023-09-13 17:02:09 -0600

    basic contact page done

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,6 +1,51 @@
 <template>
-  <div>
-    This is the contact page
+  <div class="container-fluid">
+    <section class="row justify-content-center text-center">
+      <div class="col-12">
+        <h1 class="text-secondary">Questions? Let's Chat!</h1>
+      </div>
+    </section>
+    <hr />
+    <section class="row justify-content-center">
+      <div class="col-md-5">
+        <section class="row justify-content-between">
+          <div class="col-3">
+            <img src="../assets/img/noelle.svg" class="img-fluid rounded-circle border border-light border-3 elevation" />
+          </div>
+          <div class="col-9">
+
+            <div class="m-3">
+              <p>For questions about the course content or <br /> <i>The Ultimate Guide to Selling on Etsy</i> book,
+                <br />
+                email
+                Noelle at
+                <strong>noelle@craftranker.com</strong>
+              </p>
+
+            </div>
+          </div>
+        </section>
+      </div>
+      <div class="col-md-5">
+        <section class="row justify-content-between">
+          <div class="col-3">
+            <img src="../assets/img/jeanne.svg" class="img-fluid rounded-circle border border-light border-3 elevation" />
+          </div>
+          <div class="col-9">
+
+            <div class="m-3">
+              <p>For questions about the website, <br /> your account, or any other technical questions, <br />
+                email
+                Jeanne at
+                <strong>jeanne@craftranker.com</strong>
+              </p>
+
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+
 
   </div>
 </template>
