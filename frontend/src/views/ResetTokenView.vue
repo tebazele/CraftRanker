7270d58f903a5f186a0c46d82a4d967c928a5c2a
Jeanne Allen <tebazele@gmail.com>
2023-09-17 20:20:20 -0600

    styling fixes

--- frontend/src/views/ResetTokenView.vue
+++ frontend/src/views/ResetTokenView.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    Please check your email for an email from jeanne@craftranker.com with password reset instructions :)
+  <div class="container">
+    <section class="row justify-content-center">
+      <div class="col-md-8 text-center">
+        <h3>
+          Please check your email for a note from jeanne@craftranker.com with password reset instructions :)
+
+        </h3>
+
+      </div>
+    </section>
 
   </div>
 </template>
